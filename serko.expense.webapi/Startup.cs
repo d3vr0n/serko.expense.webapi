3c2b608edf4ad1fe4a120ee16e9bb149891c5072
Avishek Acharjee <avishek.acharjee@cdk.com>
2019-08-07 10:54:33 +0530

    add new swagger-5.0, cors policy

--- serko.expense.webapi/Startup.cs
+++ serko.expense.webapi/Startup.cs
@@ -1,21 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace serko.expense.webapi
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-NZ");
@@ -23,12 +21,38 @@ namespace serko.expense.webapi
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient();
+            services.AddHttpContextAccessor();
+
+            services.AddCors(options =>
+            {
+                var allowedDomains = new[] { "http://localhost:4200" };
+
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                        .WithOrigins(allowedDomains)
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
+            //services.AddMvcCore().AddApiExplorer();
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Serko Expense API",
+                    Version = "v1"
+                });
+
+                c.DescribeAllEnumsAsStrings();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,8 +68,18 @@ namespace serko.expense.webapi
                 app.UseHsts();
             }
 
+            app.UseCors("CorsPolicy");
+
+            // insert middleware to expose the generated Swagger as JSON endpoint(s)
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Serko expense API V1");
+            });
+
             app.UseHttpsRedirection();
             app.UseMvc();
+            
         }
     }
 }
